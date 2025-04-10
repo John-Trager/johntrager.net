dcad8b42564e23a2d8273f819a44d9cfeb09f26e
John-Trager <johntrager0226@gmail.com>
2025-04-10 00:12:18 -0400

    feat: integrate Tailwind CSS and DaisyUI for styling
    - Updated package.json and package-lock.json to include Tailwind CSS and DaisyUI dependencies.
    - Added global CSS file to configure Tailwind and DaisyUI styles.
    - Created new components: BannerImage, BlogList, Navigation, and ProjectsList for better structure.
    - Implemented new layouts: JTBlogLayout and updated Layout for consistent styling.
    - Added new pages: blog, projects, and jt-blog to showcase content.
    - Enhanced the Intro component and adjusted the main index page layout.
    - Included a new background image for the blog.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://johntrager.net',
-    integrations: [sitemap()],
+  site: 'https://johntrager.net',
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
